e335a4b373c0dc1bed198ed2959f7e35a909b1f7
Braden Hayes <bradenhayes@cmail.carleton.ca>
2022-04-07 10:31:44 -0400

    Config.js

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "5cddedc548874eecbbc54089b56bae6f"; //"09d268b1a4ee42788b8a3dc24eb4f992";
+export const redirectUri =
+  "https://bradenhayes.github.io/SongPopularityContest/";
+export const scopes = [
+  "user-top-read",
+  "user-read-currently-playing",
+  "user-read-playback-state",
+  "user-library-read",
+];
