c474abc169a9496baeb08b6e6cbd53385620f96b
James <jamesruntas@cmail.carleton.ca>
2022-04-07 12:36:31 -0400

    first

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
-export const clientId = "5cddedc548874eecbbc54089b56bae6f"; //"09d268b1a4ee42788b8a3dc24eb4f992";
+export const clientId = "39f4bf5bfaf348179a03d93384530627"; //"09d268b1a4ee42788b8a3dc24eb4f992";
 export const redirectUri =
-  "https://bradenhayes.github.io/SongPopularityContest/";
+  "https://jamesruntas.github.io/SpotifyBracket/";
 export const scopes = [
   "user-top-read",
   "user-read-currently-playing",
